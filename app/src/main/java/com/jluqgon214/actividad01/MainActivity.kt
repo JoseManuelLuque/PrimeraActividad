cc3046e5514e55023903ecdbdb4d5a70f15b59c8
José Manuel Luque González <jluqgon214@g.educaand.es>
2023-09-27 15:42:29 +0200

    Avance

--- app/src/main/java/com/jluqgon214/actividad01/MainActivity.kt
+++ app/src/main/java/com/jluqgon214/actividad01/MainActivity.kt
@@ -2,10 +2,13 @@ package com.jluqgon214.actividad01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    val boton: Button = findViewById(R.id.boton)
 }
