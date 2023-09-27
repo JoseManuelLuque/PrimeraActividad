d18143e2184a0bc5fbba4647988c932b23cefe13
José Manuel Luque González <jluqgon214@g.educaand.es>
2023-09-27 16:53:22 +0200

    Finalizado

--- app/src/main/java/com/jluqgon214/actividad01/MainActivity.kt
+++ app/src/main/java/com/jluqgon214/actividad01/MainActivity.kt
@@ -1,14 +1,52 @@
 package com.jluqgon214.actividad01
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    // Declarar variables de clase
+    private lateinit var boton: Button
+    private lateinit var textView: TextView
+    private var contador = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Inicializar las variables de clase
+        boton = findViewById(R.id.boton)
+        textView = findViewById(R.id.texto)
+
+        //Acción cada vez que se pulsa el botón
+        boton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                contador++
+                actualizarTexto()
+            }
+        })
     }
 
-    val boton: Button = findViewById(R.id.boton)
+    //Funcion que actuliza el texto
+    private fun actualizarTexto() {
+        if (contador <= 5) {
+            textView.text = "Has hecho click $contador veces!"
+            textView.textSize = 24f
+        } else if (contador <= 10) {
+            textView.text = "Has hecho click varias veces ($contador)!"
+            textView.textSize = 22f
+        } else {
+            textView.text = "Te has pasado de clicks!"
+            boton.isEnabled = false
+            showToast("El botón ha sido deshabilitado")
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
 }
